88241cddf0e47c232b6458487ef0da0021fc1298
Alok Gupta <alokg252@gmail.com>
2025-06-21 19:51:38 +0530

    tried scheduling

--- src/main/java/com/example/angula/AngulaApplication.java
+++ src/main/java/com/example/angula/AngulaApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.example.reactor.components.Sweet;
 
 @ComponentScan(basePackages = {"com.example.reactor,com.example.angula"}, 
 excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Sweet.class))
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class AngulaApplication {
 
---
+++ src/main/java/com/example/reactor/jobs/LogScheduler.java
@@ -0,0 +1,29 @@
+package com.example.reactor.jobs;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "reactor.scheduler.log", havingValue = "true", matchIfMissing = true)
+public class LogScheduler {
+    
+    @Scheduled(fixedRate = 60000) // if func takes longer than 60 seconds, it will run again and cause overlap
+    public void runEveryFixed60Seconds() {
+        System.out.println("------------------------\nScheduler running every  fixed 60 seconds\n------------------------");
+    }
+    
+    @Scheduled(
+        fixedDelay = 20000, // will run after delay of 5 seconds, from the last run completion means no overlapping
+        initialDelay = 10000 // its optional arg which will wait initially after running the application then will run normally
+    )
+    public void runEveryDelay20Seconds() {
+        System.out.println("------------------------\nScheduler running every delay 20 seconds\n------------------------");
+    }
+
+    @Scheduled(cron = "0 */10 * * * *")    // seconds(0-59) minutes(0-59) hours(0-23)
+                                                           // dayOfMonth(1-31) month(1-12) dayOfWeek(0-7)->(0 or 7 is Sunday)
+    public void cronScheduler() {
+        System.out.println("------------------------\nScheduler running every cron\n------------------------");
+    }
+}
