3879ea30ef2cf8cc10c60b8d6866e13f3a20d6a1
Alok Gupta <alokg252@gmail.com>
2025-06-21 12:23:59 +0530

    undertow implemented gson removed

--- src/main/java/com/example/angula/controller/ClientRestController.java
+++ src/main/java/com/example/angula/controller/ClientRestController.java
@@ -1,11 +1,9 @@
 package com.example.angula.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import java.util.List;
 
@@ -22,46 +20,44 @@ public class ClientRestController {
     @Autowired
     AngulaUserRepo userRepo;
 
-@PostMapping
-public ResponseEntity<String> createClient(@RequestBody AngulaClient client) {
-    client.setUser(userRepo.findById(client.getUser().getId()).get());
-    clientRepo.save(client);
-    return ResponseEntity.ok("Client created");
-}
-
-@GetMapping("/{id}")
-public ResponseEntity<AngulaClient> getClientById(@PathVariable("id") Long id) {
-    return clientRepo.findById(id)
-            .map(ResponseEntity::ok)
-            .orElse(ResponseEntity.notFound().build());
-}
-
-@GetMapping
-public ResponseEntity<List<AngulaClient>> getAllClients() {
-    return ResponseEntity.ok(clientRepo.findAll());
-}
-
-@PutMapping("/{id}")
-public ResponseEntity<String> updateClient(@PathVariable("id") Long id, @RequestBody AngulaClient client) {
-    return clientRepo.findById(id).map(existingClient -> {
-        existingClient.setName(client.getName());
-        existingClient.setEmail(client.getEmail());
-        existingClient.setMobile(client.getMobile());
-        existingClient.setUser(client.getUser());
-        clientRepo.save(existingClient);
-        return ResponseEntity.ok("Client updated");
-    }).orElse(ResponseEntity.notFound().build());
-}
-
-@DeleteMapping("/{id}")
-public ResponseEntity<String> deleteClient(@PathVariable("id") Long id) {
-    if (clientRepo.existsById(id)) {
-        clientRepo.deleteById(id);
-        return ResponseEntity.ok("Client deleted");
-    } else {
-        return ResponseEntity.notFound().build();
+    @PostMapping
+    public ResponseEntity<String> createClient(@RequestBody AngulaClient client) {
+        client.setUser(userRepo.findById(client.getUser().getId()).get());
+        clientRepo.save(client);
+        return ResponseEntity.ok("Client created");
     }
-}
 
-    
+    @GetMapping("/{id}")
+    public ResponseEntity<AngulaClient> getClientById(@PathVariable("id") Long id) {
+        return clientRepo.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AngulaClient>> getAllClients() {
+        return ResponseEntity.ok(clientRepo.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateClient(@PathVariable("id") Long id, @RequestBody AngulaClient client) {
+        return clientRepo.findById(id).map(existingClient -> {
+            existingClient.setName(client.getName());
+            existingClient.setEmail(client.getEmail());
+            existingClient.setMobile(client.getMobile());
+            existingClient.setUser(client.getUser());
+            clientRepo.save(existingClient);
+            return ResponseEntity.ok("Client updated");
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteClient(@PathVariable("id") Long id) {
+        if (clientRepo.existsById(id)) {
+            clientRepo.deleteById(id);
+            return ResponseEntity.ok("Client deleted");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
