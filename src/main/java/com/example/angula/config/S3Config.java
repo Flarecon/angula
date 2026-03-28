eb2d14245c6d776d1e55bee51205260e7848c7cd
Alokg252 <alokg252@gmail.com>
2026-03-28 11:56:38 +0530

    added prod config demo

--- src/main/java/com/example/angula/config/S3Config.java
+++ src/main/java/com/example/angula/config/S3Config.java
@@ -3,7 +3,9 @@ package com.example.angula.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -11,14 +13,11 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.creds.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.creds.secret-key}")
-    private String secretKey;
-
     @Bean
-    public S3Client s3Client() {
+    @Profile("dev")
+    public S3Client s3Client(
+            @Value("${cloud.aws.creds.access-key}") String accessKey,
+            @Value("${cloud.aws.creds.secret-key}") String secretKey) {
 
         AwsBasicCredentials credentials = AwsBasicCredentials
                 .create(accessKey, secretKey);
@@ -26,6 +25,19 @@ public class S3Config {
         return S3Client.builder()
                 .region(Region.AP_SOUTH_1)
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+
+    // its used for aws service to service communication (for example maybe if i deploy it later)
+    // a Ec2 or any service in which this backend is running just needs a Role with s3 permissions
+    // to access this bean not access/secret credentials are needed cause we are already in
+
+    @Bean
+    @Profile("prod")
+    public S3Client s3ClientProd() {
+        return S3Client.builder()
+                .region(Region.AP_SOUTH_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
