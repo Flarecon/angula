71ff3e7526358aa500c654a3e662db9de4f27301
Alokg252 <alokg252@gmail.com>
2026-06-04 22:10:19 +0530

    kafka not working

--- src/main/java/com/example/angula/services/kafka/KafkaConsumerService.java
+++ src/main/java/com/example/angula/services/kafka/KafkaConsumerService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
 
-    @KafkaListener(topics = Constants.KAFKA_TOPIC)
+//    @KafkaListener(topics = Constants.KAFKA_TOPIC)
     public void consume(String message) {
         System.out.printf("<-- Received message: %s%n", message);
     }
